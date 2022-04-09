8d13f2986f015405654cac503a3823cf84225e54
Juan Tombeng <juantombeng03@gmail.com>
2022-04-09 20:21:47 +0700

    db connection closed error

--- src/v1/config/dbConfig.js
+++ src/v1/config/dbConfig.js
@@ -1,18 +1,18 @@
 const mysql = require('mysql2')
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host : process.env.DB_HOST,
     user : process.env.DB_USERNAME,
     password : process.env.DB_PASSWORD,
     database : process.env.DB_NAME
 })
 
-connection.connect((err) => {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log('Connected to MySQL Server!');
-    }
-});
+// connection.connect((err) => {
+//     if (err) {
+//         console.log(err)
+//     } else {
+//         console.log('Connected to MySQL Server!');
+//     }
+// });
 
 module.exports = connection
